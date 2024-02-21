6035e88409735f213cf287a19eedcf542f9e0eb2
jeseias <domingosjeseias@gmail.com>
2024-02-21 17:23:25 +0100

    chore: add editorconfig

--- infra/src/schema.rs
+++ infra/src/schema.rs
@@ -1,11 +1,11 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    posts (id) {
-        id -> Int4,
-        title -> Varchar,
-        body -> Text,
-        genre -> Varchar,
-        published -> Bool,
-    }
+  posts (id) {
+    id -> Int4,
+    title -> Varchar,
+    body -> Text,
+    genre -> Varchar,
+    published -> Bool,
+  }
 }
